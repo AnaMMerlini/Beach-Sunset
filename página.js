0644a8f38ba38f4cf921af65b46fb7eb3ea7ed4f
Ana Merlini <vrlrosamargarita@gmail.com>
2024-06-08 19:30:53 -0300

    javascript y modificaciones

---
+++ página.js
@@ -0,0 +1,225 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img1").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img1").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img2").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img2").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img3").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img3").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img4").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img4").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img5").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img5").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img6").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img6").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img7").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img7").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img8").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img8").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img9").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img9").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img10").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img10").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img11").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img11").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img12").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img12").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img13").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img13").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img14").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img14").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img15").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img15").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img16").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img16").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img17").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img17").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img18").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img18").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img19").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img19").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img20").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img20").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img21").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img21").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img22").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img22").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img23").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img23").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".img24").addEventListener("mouseover", function() {
+        $(this).css("box-shadow", "rgb(150,150,150) 5px 5px 10px");
+    });
+    document.querySelector(".img24").addEventListener("mouseout", function() {
+        $(this).css("box-shadow", "none");
+    });
+});
+
+function abrirpag() {
+    var url = "contacto.html";
+    window.open(url);
+}
+
+function abrirpag() {
+    var url = "Beachsunset.html";
+    window.open(url);
+}
